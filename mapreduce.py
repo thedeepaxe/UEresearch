c9914c05df5c898bc3e2545341e50598207933cd
karim <102855315+thedeepaxe@users.noreply.github.com>
2024-12-12 18:01:00 +0100

    update path

--- mapreduce.py
+++ mapreduce.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     initialize_csv(log_file)  # Create the CSV file with headers
     
     # Load the input text
-    with open("eronEmailDatabase/emails.csv", 'r') as file:
+    with open("testFiles/eronEmailDatabase/emails.csv", 'r') as file:
         input_text = file.read()
     
     # Experiment with different configurations
